f2d047458b7da649be0b9dc795d4d2d7c5eb322d
unknown <smulgir636@gmail.com>
2023-04-13 20:12:56 +0530

    adding main and RWOnly classes

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    void setName(String s){
+        this.name = s;
+    }
+    String getName(){
+        return name;
+    }
     
 }
