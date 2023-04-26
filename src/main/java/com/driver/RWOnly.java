af5998aa890fb3c606a12254388a4b42b521c83f
SachinMulgir <smulgir636@gmail.com>
2023-04-27 02:42:37 +0530

    commit 2

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,10 +3,10 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    void setName(String s){
+    public void setName(String s){
         this.name = s;
     }
-    String getName(){
+    public String getName(){
         return name;
     }
     
